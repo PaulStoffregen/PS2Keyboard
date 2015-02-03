a41b8c717314800b9d69694f5ddb40dcb2320c38
PaulStoffregen <paul@pjrc.com>
2015-02-03 04:00:48 -0800

    Support more interrupt pins

--- PS2Keyboard.cpp
+++ PS2Keyboard.cpp
@@ -452,6 +452,86 @@ void PS2Keyboard::begin(uint8_t data_pin, uint8_t irq_pin, const PS2Keymap_t &ma
       irq_num = 7;
       break;
     #endif
+    #ifdef CORE_INT8_PIN
+    case CORE_INT8_PIN:
+      irq_num = 8;
+      break;
+    #endif
+    #ifdef CORE_INT9_PIN
+    case CORE_INT9_PIN:
+      irq_num = 9;
+      break;
+    #endif
+    #ifdef CORE_INT10_PIN
+    case CORE_INT10_PIN:
+      irq_num = 10;
+      break;
+    #endif
+    #ifdef CORE_INT11_PIN
+    case CORE_INT11_PIN:
+      irq_num = 11;
+      break;
+    #endif
+    #ifdef CORE_INT12_PIN
+    case CORE_INT12_PIN:
+      irq_num = 12;
+      break;
+    #endif
+    #ifdef CORE_INT13_PIN
+    case CORE_INT13_PIN:
+      irq_num = 13;
+      break;
+    #endif
+    #ifdef CORE_INT14_PIN
+    case CORE_INT14_PIN:
+      irq_num = 14;
+      break;
+    #endif
+    #ifdef CORE_INT15_PIN
+    case CORE_INT15_PIN:
+      irq_num = 15;
+      break;
+    #endif
+    #ifdef CORE_INT16_PIN
+    case CORE_INT16_PIN:
+      irq_num = 16;
+      break;
+    #endif
+    #ifdef CORE_INT17_PIN
+    case CORE_INT17_PIN:
+      irq_num = 17;
+      break;
+    #endif
+    #ifdef CORE_INT18_PIN
+    case CORE_INT18_PIN:
+      irq_num = 18;
+      break;
+    #endif
+    #ifdef CORE_INT19_PIN
+    case CORE_INT19_PIN:
+      irq_num = 19;
+      break;
+    #endif
+    #ifdef CORE_INT20_PIN
+    case CORE_INT20_PIN:
+      irq_num = 20;
+      break;
+    #endif
+    #ifdef CORE_INT21_PIN
+    case CORE_INT21_PIN:
+      irq_num = 21;
+      break;
+    #endif
+    #ifdef CORE_INT22_PIN
+    case CORE_INT22_PIN:
+      irq_num = 22;
+      break;
+    #endif
+    #ifdef CORE_INT23_PIN
+    case CORE_INT23_PIN:
+      irq_num = 23;
+      break;
+    #endif
   }
 #endif
 
